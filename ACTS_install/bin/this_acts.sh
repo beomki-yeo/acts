7a52b4bfafeb72c6c084c07bdaf209981b5c6864
bomki <bomki@localhost.localdomain>
2020-03-20 17:31:57 +0900

    Delete Acts install

--- ACTS_install/bin/this_acts.sh
+++
@@ -1,5 +0,0 @@
-# set up environment variables
-# the ${VAR:+:} part adds a double colon only if VAR is not empty
-export PATH="/home/bomki/Projects/ACTS/ACTS_install/bin${PATH:+:}${PATH}"
-export LD_LIBRARY_PATH="/home/bomki/Projects/ACTS/ACTS_install/lib64${LD_LIBRARY_PATH:+:}${LD_LIBRARY_PATH}"
-export DYLD_LIBRARY_PATH="/home/bomki/Projects/ACTS/ACTS_install/lib64${DYLD_LIBRARY_PATH:+:}${DYLD_LIBRARY_PATH}"
